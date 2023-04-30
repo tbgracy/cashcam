f64044b58d40a414884abaada1d88e9d824c879d
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2023-04-30 10:57:26 +0300

    feat: Change camera preview appearence

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:camera/camera.dart';
-import 'package:cashcam/constants.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/camera_action_container_widget.dart';
@@ -42,45 +41,62 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            child: FutureBuilder(
-              future: _initializeControllerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return CameraPreview(_cameraController);
-                } else {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
-          ),
-          Positioned(
-            top: 100,
-            child: Container(
-              width: 300,
-              height: 80,
-              decoration: BoxDecoration(
-                color: colors[1].withAlpha(90),
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(
-                  color: colors[1],
-                  width: 3,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Positioned(
+              top: 100,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: FutureBuilder(
+                    future: _initializeControllerFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 120,
+                          child: ClipRRect(
+                            child: OverflowBox(
+                              alignment: Alignment.topCenter,
+                              child: FittedBox(
+                                alignment: Alignment.topCenter,
+                                fit: BoxFit.cover,
+                                child: SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  // height: MediaQuery.of(context).size.height / _cameraController.value.aspectRatio,
+                                  child: GestureDetector(
+                                    onTap: () async {
+                                      await _cameraController
+                                          .setFocusPoint(null);
+                                    },
+                                    child: CameraPreview(_cameraController),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 40,
-            child: CameraActionContainerWidget(_cameraController),
-          ),
-        ],
+            Positioned(
+              bottom: 40,
+              child: CameraActionContainerWidget(_cameraController),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/services/photo_service.dart
+++ lib/services/photo_service.dart
@@ -3,14 +3,17 @@ import 'dart:io';
 import 'package:flutter_native_image/flutter_native_image.dart';
 
 class PhotoService {
-  Future<String> getCroppedImagePath(String filePath, int originX, int originY) async {
+  Future<String> getCroppedImagePath(String filePath, int originX, int originY, int sHeight) async {
+    final properties = await FlutterNativeImage.getImageProperties(filePath);
+    int pWidth = properties.height!;
+    int pHeight = 170;
+
     File croppedFile = await FlutterNativeImage.cropImage(
       filePath,
-      // filePath.split('.').first + DateTime.now().toIso8601String() + '.' + filePath.split('.').last,
       originX,
       originY,
-      300,
-      90,
+      pHeight,
+      pWidth,
     );
 
     return croppedFile.path;
